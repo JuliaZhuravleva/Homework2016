36d9cb5a7a7e40debd95c6502164634a4426e5e5
student <student@eprupetw6642.petersburg.epam.com>
2016-03-29 21:49:10 +0300

    task is done

---
+++ homework/src/lesson160329/task01.java
@@ -0,0 +1,10 @@
+package lesson160329;
+
+public class task01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println(1+2+3+4+5+6+7+8+9+10);
+	}
+
+}
