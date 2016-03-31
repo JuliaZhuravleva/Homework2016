7ab84a4b3c40090290de8e203267e8ae34754a9e
student <student@eprupetw6642.petersburg.epam.com>
2016-03-31 22:02:59 +0300

    operators

---
+++ homework/src/lesson160331/PrimitiveTypes.java
@@ -0,0 +1,54 @@
+package lesson160331;
+
+public class PrimitiveTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		byte b = 1; // 8 bits = 1 byte
+		b = Byte.MAX_VALUE;
+		b = Byte.MIN_VALUE;
+		
+		
+		
+		short s = 34; // 16 bits == 2 bytes
+		s=Short.MAX_VALUE;
+		s=Short.MIN_VALUE;
+		
+		
+	
+		int i = 10; // 32 bits == 4 bytes
+		i = Integer.MAX_VALUE;
+		i = Integer.MIN_VALUE;
+		
+		
+		
+		long l = 20L; // 64 bits == 8 bytes
+		l = Long.MAX_VALUE;
+		l = Long.MIN_VALUE;
+		
+		
+		
+		char c = 65; // 16 bits == 2 bytes // ASCII code
+		c = Character.MAX_VALUE;
+		c = Character.MIN_VALUE;
+		
+		
+		
+		float f = 16f;  // 32 bits == 4 bytes
+		f = 10;
+		System.out.println(f);
+		f = Float.MAX_VALUE;
+		f = Float.MIN_VALUE;
+		
+		
+		double d = 12.2; // 64 bits == 8 bytes
+		d = Double.MAX_VALUE;
+		d = Double.MIN_VALUE;
+		
+				
+				
+
+	}
+
+}
