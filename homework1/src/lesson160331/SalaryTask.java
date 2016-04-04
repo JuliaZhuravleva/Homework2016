3536333379e2972728d48cda066d074ca4b58f03
student <student@eprupetw6642.petersburg.epam.com>
2016-04-04 19:44:23 +0300

    task is done

---
+++ homework1/src/lesson160331/SalaryTask.java
@@ -0,0 +1,13 @@
+package lesson160331;
+
+public class SalaryTask {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int salary = 26490;
+double tax = 0.13;
+double finaly = salary - tax * salary;
+System.out.println(finaly);
+	}
+
+}
